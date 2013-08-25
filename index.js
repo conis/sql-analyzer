3d865c4fee230788d6aa72544823214f0863baa0
Conis <conis.yi@gmail.com>
2013-08-25 21:09:13 +0800

    加入对sqlite进行分析的功能
    可以直接读取sqlite数据库进行分析

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var _ = require("underscore");
 var _s = require('underscore.string');
-
 //分析并提取表名
 var extractTableName = function(sql){
 	var pattern = /(.+)?\s([a-zA-z0-9_]{1,})\(.+/ig;
@@ -47,4 +46,32 @@ exports.analyseWithSql = function(sql){
 		tableName: extractTableName(sql),
 		fields: extractFields(sql)
 	}
+}
+
+//根据sqlite进行分析，tables可选为string或者array
+//@params path sqlite的文件路径
+//@parmas tables 要分析的表，可选为string或array。如果不选择此参数，则查询所有表，当不选择此参数时，可以是callback
+//@params callback 完成后的回调
+exports.analyseWithSqlite = function(path, tables, callback){
+	if(typeof(tables) == 'function') callback = tables;
+
+	var sqlite3 = require('sqlite3').verbose();
+	var db = new sqlite3.Database(path);
+
+	var result = [];
+	db.serialize(function() {
+		//读取所有的表
+		var sql = "SELECT sql FROM sqlite_master WHERE type = 'table' AND tbl_name <> 'sqlite_sequence'";
+		if(tables instanceof  Array) tables = tables.join("','");
+		if(tables) sql += _s.sprintf(" AND tbl_name in ('%s')", tables);
+
+		db.each(sql, function(err, row){
+			if(!err){
+				result.push(exports.analyseWithSql(row.sql));
+			}
+		}, function(){
+			callback(null, result);
+		})
+	});
+	db.close();
 }
